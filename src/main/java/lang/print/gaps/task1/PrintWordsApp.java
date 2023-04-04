8fb8123cd037a32d9b2ccdb1102522dbfc8a85f3
EPAM\Ajitha_Nalluri <Ajitha_Nalluri@epam.com>
2023-04-04 11:26:07 +0530

    #1 solution

--- src/main/java/lang/print/gaps/task1/PrintWordsApp.java
+++ src/main/java/lang/print/gaps/task1/PrintWordsApp.java
@@ -1,7 +1,9 @@
 package lang.print.gaps.task1;
 
+import java.util.Arrays;
+
 public class PrintWordsApp {
     public static void main(String[] args) {
-        //write your code here
+        System.out.println( "java" + "\n" + "is"+ "\n" +"smth");
     }
 }
